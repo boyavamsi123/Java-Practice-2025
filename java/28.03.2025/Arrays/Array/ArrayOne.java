9d10f8af78dbcf6b5b1dbc0dae86a14f73d0a9bf
boyavamsi123 <vamvamshi56@gmail.com>
2025-03-28 13:15:51 +0530

    check

---
+++ java/28.03.2025/Arrays/Array/ArrayOne.java
@@ -0,0 +1,38 @@
+public class ArrayOne{
+    public static void main(String[] args){
+        /*int[] j = new int[5];
+        j[0] = 5;
+        j[2] = 8;
+        j[4] = 45;
+
+        j[2] = 34;
+        j[2] = 85;*/
+
+       int[] j={5,8,5,9,1};
+
+       /*int[] j;
+       j= new int[] {45,85,96,32,10};*/
+
+       System.out.println("size is "+ j.length);
+       for(int i=0; i<j.length;i++){
+        System.out.println(j[i]);
+       }
+
+       /*for(int i=0; i<j.length;i++){
+            System.out.println(j[i]);
+        } 
+
+        System.out.println("=====================");
+
+       int sum = j[0] + j[1] + j[2] +j[3] +j[4];
+       System.out.println("Sum of : "+sum); 
+
+       System.out.println("=======================");
+
+       int i=0;
+       while(i<j.length){
+        System.out.println(j[i]);
+        i++;
+       }*/
+    }
+}
