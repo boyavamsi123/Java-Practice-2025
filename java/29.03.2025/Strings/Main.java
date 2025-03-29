5456a1d60732b4055d2eb472c9379a588d92cbd8
boyavamsi123 <vamvamshi56@gmail.com>
2025-03-29 23:40:50 +0530

    check

---
+++ java/29.03.2025/Strings/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args){
+        short i=20000;
+        String s1 = "Yadagiri Reddy";
+        boolean b1 = true;
+
+        System.out.println(i);  //20000
+        System.out.println(s1); //Yadagiri Reddy
+        System.out.println(b1); //true
+
+        int var = 10;
+
+        System.out.println(var);
+
+        int[] arr = new int[5];
+        arr[0]= 1;
+
+    }
+}
