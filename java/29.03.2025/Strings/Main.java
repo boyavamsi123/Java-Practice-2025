4f583ea012e6840250dae2c1f8f091e3ce9498e6
boyavamsi123 <vamvamshi56@gmail.com>
2025-03-31 06:44:43 +0530

    check

--- java/29.03.2025/Strings/Main.java
+++ java/29.03.2025/Strings/Main.java
@@ -9,11 +9,17 @@ public class Main{
         System.out.println(b1); //true
 
         int var = 10;
-
-        System.out.println(var);
-
-        int[] arr = new int[5];
-        arr[0]= 1;
-
+        var = var + var;
+        System.out.println(var); 
+       // int[] arr = new int[5];
+       // arr[0]= 1;
+        var arr =  new int[]{1,2,3};
+        var arr2 = new int[3];
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 1;
+    }
+    public void add(int i,int j){
+        System.out.println(i+j);
     }
 }
